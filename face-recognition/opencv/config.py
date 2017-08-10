a4e3c2ace2b2579788ca724e2e2421bf56db6b0b
Jason Noer <jnoer@widen.com>
2017-08-10 15:34:26 -0500

    d

--- face-recognition/opencv/config.py
+++ face-recognition/opencv/config.py
@@ -34,6 +34,7 @@ ANAMARIA_LABEL = 7
 OLIVIER_LABEL = 8
 NADINE_LABEL = 9
 JASON_LABEL = 10
+NICO_LABEL = 11
 
 # Size (in pixels) to resize images for training and prediction.
 # Don't change this unless you also change the size of the training images.
